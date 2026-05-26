bc522f879be8f5f6be816e28be6d85c28e1fd274
Chitransh27309-crypto <ayush27309@gmail.com>
2026-05-26 22:26:34 +0530

    Add Restart Button

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let msg = document.querySelector("#msg");
 const winningPatern = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 let turnO = true;
-
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnO) {
             box.innerHTML = "O";
+            box.style.color = "rgb(222 43 43 / 88%)"
             turnO = false;
         }
         else {
@@ -18,7 +18,7 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
-
+        
         checkWinner();
     })
 })
@@ -40,9 +40,16 @@ let checkWinner = () => {
                 disable();
                 msg.innerHTML = `Winner is ${pos1Val}`;
                 msgContainer.classList.remove("hide");
-            
             }
         }
-
     }
 }
+let restartGame = () => {
+    for (const box of boxes) {
+        box.innerHTML = "";
+        box.disabled = false;
+    }
+    msgContainer.classList.add("hide");
+    turnO = true;
+}
+restartbtn.addEventListener("click", restartGame)
